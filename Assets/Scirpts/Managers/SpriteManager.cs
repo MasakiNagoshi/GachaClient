0de08d0c55e564fd374a8572a298bc8f49998dd1
YUUTO\大樹 <yakmsy.memories@ezweb.ne.jp>
2018-04-17 19:37:12 +0900

    SpriteManagerの追加

---
+++ Assets/Scirpts/Managers/SpriteManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpriteManager
+{
+    Sprite[] sprites;
+    public enum ReadStatus
+    {
+        Tickets,
+    }
+
+    static SpriteManager instance;
+    public SpriteManager(ReadStatus status)
+    {
+        instance = this;
+        ReadData(status);
+    }
+    public static SpriteManager Instance{ get { return instance; } }
+    void ReadData(ReadStatus status)
+    {
+        switch(status)
+        {
+            case ReadStatus.Tickets:
+                sprites = Resources.LoadAll<Sprite>("Image/Ticket");
+                break;
+        }
+    }
+
+    public Sprite GetSprite(int number)
+    {
+        return sprites[number];
+    }
+}
